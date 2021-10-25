b67709a86be195478afc19ce56b3762820c03d57
wrsmA <36059952+wrsmA@users.noreply.github.com>
2021-10-25 15:04:09 +0900

    add solution

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(params string[] args)
+        {
+
+            return;
+        }
+    }
+}
